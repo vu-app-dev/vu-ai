328aa8462966d73a9526ebcbb56b72f3cc1c2a30
om4rrr <omar.2015.oa651@gmail.com>
2026-02-04 16:35:29 +0200

    create stt_service and streamingSTT_service

---
+++ services/__init__.py
@@ -0,0 +1,4 @@
+from .stt.stt_service import STT, Transcription
+from .stt.stt_realtime import RealtimeSTT
+
+__all__ = ["STT", "Transcription", "RealtimeSTT"]
---
+++ services/stt/__init__.py
@@ -0,0 +1,4 @@
+from .stt_service import STT, Transcription
+from .stt_realtime import RealtimeSTT
+
+__all__ = ["STT", "Transcription", "RealtimeSTT"]
